a9a3fc776ba986aa9b584c8fb9a3e8a30d8d3afe
Varada Kolhatkar <kvarada@cs.ubc.ca>
2019-02-15 13:39:57 -0800

    adding feature3

--- source/test.py
+++ source/test.py
@@ -6,3 +6,6 @@ nvowels = 0
 for vowel in vowels: 
 	nvowels += name.count(vowel)
 print('You have %d vowels in your name ' % nvowels )
+
+nconsonants = len(name) - nvowels 
+print('You have %d consonants in your name ' %nconsonants)
