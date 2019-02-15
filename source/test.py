3c066153469114b28ecaf7f7c5b7b72cb35d6f87
Varada Kolhatkar <kvarada@cs.ubc.ca>
2019-02-15 13:29:38 -0800

    adding feature2

--- source/test.py
+++ source/test.py
@@ -1,2 +1,8 @@
 name = input('Enter you name: ')
 print('Hello ', name)
+
+vowels = ['a','e','i','o','u']
+nvowels = 0
+for vowel in vowels: 
+	nvowels += name.count(vowel)
+print('You have %d vowels in your name ' % nvowels )
