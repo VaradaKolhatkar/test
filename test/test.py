2df6a171d8005b338d76780497cbdb5148b86183
Varada Kolhatkar <kvarada@cs.ubc.ca>
2019-02-15 16:29:22 -0800

    minor changes

--- test/test.py
+++ test/test.py
@@ -1,8 +1,17 @@
-name = input('Enter you name: ')
-print('Hello ', name)
 
-vowels = ['a','e','i','o','u']
-nvowels = 0
-for vowel in vowels: 
-	nvowels += name.count(vowel)
-print('You have %d vowels in your name ' % nvowels )
+class Simple:
+
+	def __init__(self):
+		self.name = ''	
+
+	def get_name(self):
+		self.name = input('Enter you name: ')
+		print('Hello ', self.name)
+
+	def count_vowels(self):
+		vowels = ['a','e','i','o','u']
+		nvowels = 0
+		for vowel in vowels: 
+			nvowels += self.name.count(vowel)
+		print('You have %d vowels in your name ' % nvowels )
+
